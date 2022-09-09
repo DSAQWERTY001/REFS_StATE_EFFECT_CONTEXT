fecc791469437ce71465a3d46144a9f470f8798c
werty <dsaqwerty001@gmail.com>
2022-09-09 11:38:30 +0700

    state

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,10 +1,11 @@
-import Eventdata2 from './event-data';
+
 import './App.css';
-import Banner from './banner';
 import RefsArray from './refs-array';
+import MessageBox from './state-func';
 function App() {
   return <div className='App'>
             <RefsArray/>
+            <MessageBox/>
           </div>
 }
 
